d7ea936c8de5a292747f6467cb8625517b231066
gprasadsowmya <gprasadsowmya@gmail.com>
2025-03-18 21:34:47 -0700

    Update app.js
    correct viz url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const vizUrl = 'https://public.tableau.com/app/profile/sowmya.prasad/viz/NYCRestaurantsDashboard_17423435798770/Dashboard1?publish=yes';
+const vizUrl = 'https://public.tableau.com/views/NYCRestaurantsDashboard_17423435798770/Dashboard1?:language=en-GB&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
 
 function initViz() {
     const containerDiv = document.getElementById('vizContainer');
